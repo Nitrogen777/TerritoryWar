732143595e683e7e673d7167abef88222c609e65
EyShap <eyalm1000@gmail.com>
2021-03-04 11:14:16 +0200

    -Fixed quit

--- GameUtils.py
+++ GameUtils.py
@@ -177,7 +177,7 @@ class Player:
             pygame.mixer_music.play(0)
             while True:
                 for event in pygame.event.get():
-                    if event == pygame.QUIT:
+                    if event.type == pygame.QUIT:
                         pygame.quit()
                         exit(11)
         if not self.is_computer:
